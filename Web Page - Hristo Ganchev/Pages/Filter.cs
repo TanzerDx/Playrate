84ce54a23d7112ff6c869b5ebe8f1209b13b686f
Hristo <i499630@fhict.nl>
2023-03-10 02:13:46 +0100

    Used some Model Binding for the search function

---
+++ Web Page - Hristo Ganchev/Pages/Filter.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.Metrics;
+using System.Xml.Linq;
+
+namespace Home_Page___Hristo_Ganchev
+{
+    public class Filter
+    {
+        public Filter()
+        { }
+
+        public string Keyword { get; set; }
+
+        public Filter(string keyword)
+        {
+            Keyword = keyword;
+        }
+
+        public string GetKeyword()
+        {
+            return Keyword;
+        }
+    }
+}
--- Web Page - Hristo Ganchev/Pages/GamesPage.cshtml.cs
+++ Web Page - Hristo Ganchev/Pages/GamesPage.cshtml.cs
@@ -5,8 +5,32 @@ namespace Home_Page___Hristo_Ganchev
 {
     public class GamesPageModel : PageModel
     {
+
+        public string PageTitle { get; set; }
+
+        public string SubmittedKeyword { get; set; }
+
+        [BindProperty]
+        public Filter Filter { get; set; }
+
+        public GamesPageModel()
+        {
+            PageTitle = "GAMES:";
+        }
+
         public void OnGet()
         {
+
+        }
+
+
+        public void OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                SubmittedKeyword = $"{Filter.GetKeyword()}";
+            }
+
         }
     }
 }
